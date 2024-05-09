a6c05da51e4dbc07057890adf345a7f0b5c73ae3
Euroteleport <second1sunny@gmail.com>
2024-05-09 21:28:07 +0200

    Test pour Anthony

--- La derniere lueur de Fritz/Assets/Script/deplacementFritz.cs
+++ La derniere lueur de Fritz/Assets/Script/deplacementFritz.cs
@@ -3,7 +3,11 @@ using UnityEngine;
 public class deplacementFritz : MonoBehaviour
 {
     public float moveSpeed;
+    public float jumpForce;
+    public bool isJumping;
     public Rigidbody2D rb;
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
     
     private Vector3 velocity = Vector3.zero;
     
@@ -12,14 +16,43 @@ public class deplacementFritz : MonoBehaviour
     void FixedUpdate()
     {
         float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-
+         
+         if(Input.GetButtonDown("Jump"))
+         {
+           isJumping = true;
+         }
+             
+    
          MovePlayer(horizontalMovement);
+
+         flip(rb.velocity.x);
+
+         float characterVelocity = Mathf.Abs(rb.velocity.x);
+         animator.SetFloat("Speed", characterVelocity);
     }
 
     void MovePlayer(float _horizontalMovement)
     {
         Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.velocity.y);
         rb.velocity =  Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
+
+        if(isJumping == true)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce));
+            isJumping = false;
+        }
+    }
+
+    void flip(float _velocity)
+    {
+      if(_velocity > 0.1f)
+      {
+        spriteRenderer.flipX = false;
+      }else if(_velocity < -0.1f)
+      {
+        spriteRenderer.flipX = true;
+      }
     }
 }
+
 
